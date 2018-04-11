83988dcfec21945280b8a68d7d256deb3fbc44a2
lipenghui <365464919@qq.com>
2018-04-11 15:20:00 +0800

    修复mac地址带\n的bug
    修复mac地址带\n的bug

--- src/main.c
+++ src/main.c
@@ -149,6 +149,7 @@ int main(int argc, char* argv[])
 	//获取每个设备Topic的根节点
 	if(getmac(mac) == 0)
 	{
+		mac[17] = '0';
         sprintf(TOPICROOT, "/%s/", mac);    
 	}
     pthread_create(&threads[0], NULL, subClient, NULL);
