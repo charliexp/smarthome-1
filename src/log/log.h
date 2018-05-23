12972fa80422ecd09a30c5c6ed2666449a0cc3c6
lipenghui <365464919@qq.com>
2018-05-23 08:40:00 +0800

    代码提交

--- src/log/log.h
+++ src/log/log.h
@@ -1,13 +1,13 @@
 #ifndef _LOG_H_
 #define _LOG_H_
 
-#define LOG_DEBUG(format..) if(g_debug_level == 0) my_log(##format);\
+#define LOG_DEBUG(format...) if(g_debug_level == 0) my_log(##format);\
     else do{}while(0);
 
-#define LOG_INFO(format..)  if(g_debug_level == 0 || g_debug_level == 1) my_log(##format);\
+#define LOG_INFO(format...)  if(g_debug_level == 0 || g_debug_level == 1) my_log(##format);\
     else do{}while(0);
 
-#define LOG_ERROR(format..)  if(g_debug_level == 0 || g_debug_level == 1 || g_debug_level == 2) my_log(##format);\
+#define LOG_ERROR(format...)  if(g_debug_level == 0 || g_debug_level == 1 || g_debug_level == 2) my_log(##format);\
     else do{}while(0);
 
 #endif
