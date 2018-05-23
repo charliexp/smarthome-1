56878119a028444e8bde6360697619a952cc2e9c
lipenghui <365464919@qq.com>
2018-05-23 09:16:04 +0800

    代码提交

--- src/log/log.h
+++ src/log/log.h
@@ -1,20 +1,19 @@
 #ifndef _LOG_H_
 #define _LOG_H_
 
-int g_debug_level = 0;
-
+extern int g_log_level;
 void my_log(const char *format, ...);
 
 int log_init();
 
 
-#define MYLOG_DEBUG(format...) if(g_debug_level == 0) my_log(format);\
+#define MYLOG_DEBUG(format...) if(g_log_level == 0) my_log(format);\
     else do{}while(0);
 
-#define MYLOG_INFO(format...)  if(g_debug_level == 0 || g_debug_level == 1) my_log(format);\
+#define MYLOG_INFO(format...)  if(g_log_level == 0 || g_log_level == 1) my_log(format);\
     else do{}while(0);
 
-#define MYLOG_ERROR(format...)  if(g_debug_level == 0 || g_debug_level == 1 || g_debug_level == 2) my_log(format);\
+#define MYLOG_ERROR(format...)  if(g_log_level == 0 || g_log_level == 1 || g_log_level == 2) my_log(format);\
     else do{}while(0);
 
 #endif
