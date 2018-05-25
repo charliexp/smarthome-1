e742399e55c1daba872a4fde4623bf7373d3c9a4
lipenghui <365464919@qq.com>
2018-05-25 14:09:36 +0800

    修改编译bug

--- src/device/device.h
+++ src/device/device.h
@@ -19,7 +19,7 @@ int freshaircontrol(cJSON *device, char packetid);
 int socketcontrol(cJSON *device, char packetid);
 
 /*zigbee消息发送接口*/
-int sendzgbmsg(ZGBADDRESS address, BYTE *data, char length, char msgtype, char devicetype, char deviceindex, char packetid)
+int sendzgbmsg(ZGBADDRESS address, BYTE *data, char length, char msgtype, char devicetype, char deviceindex, char packetid);
 
 
 
