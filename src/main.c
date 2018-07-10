f8e42141befddc24a2eece21bcf9769e4e1ce686
liph1988 <365464919@qq.com>
2018-07-10 16:23:33 +0800

    代码修改

--- src/main.c
+++ src/main.c
@@ -501,7 +501,7 @@ void* uartsend(void *argc)
 			MYLOG_ERROR("recive uartsendmsg fail!");
 		}
         MYLOG_INFO("uart begin to send msg!");
-        MYLOG_BYTE(qmsg.msg, sizeof(qmsg.msg));
+        MYLOG_BYTE((BYTE*)&qmsg.msg, sizeof(qmsg.msg));
         if ( write(g_uartfd, (char *)&qmsg.msg, (int)(qmsg.msg.msglength + 2)) != (qmsg.msg.msglength + 2))
 	    {
 		    MYLOG_ERROR("com write error!");
