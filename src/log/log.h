3cd9d1b537ea9883b107c98eb3bc0ac747558de4
liph1988 <365464919@qq.com>
2018-07-26 16:49:46 +0800

    代码提交

--- src/log/log.h
+++ src/log/log.h
@@ -19,19 +19,19 @@ int log_init();
 
 #define MYLOG_ERROR(format...)  my_log((char*)__FILE__, (int)__LINE__, (char*)__FUNCTION__, format)
 
-#define MYLOG_ZGBMSG(msg) {  MYLOG_INFO("header:%2X", msg.header);MYLOG_INFO("msglength:%2X", msg.msglength);\
-    MYLOG_INFO("framecontrol:%2X%2X", msg.payload.framecontrol[0], msg.payload.framecontrol[1]);\
-    MYLOG_INFO("src:%2X%2X%2X%2X%2X%2X%2X%2X", msg.payload.src[0], msg.payload.src[1], msg.payload.src[2], msg.payload.src[3], msg.payload.src[4], msg.payload.src[5], msg.payload.src[6], \
+#define MYLOG_ZGBMSG(msg) {  MYLOG_INFO("header:%2X", msg->header);MYLOG_INFO("msglength:%2X", msg->msglength);\
+    MYLOG_INFO("framecontrol:%2X%2X", msg->payload->framecontrol[0], msg->payload->framecontrol[1]);\
+    MYLOG_INFO("src:%2X%2X%2X%2X%2X%2X%2X%2X", msg->payload->src[0], msg->payload->src[1], msg->payload->src[2], msg->payload->src[3], msg->payload->src[4], msg->payload->src[5], msg->payload->src[6], \
     msg.payload.src[7]);\
-    MYLOG_INFO("src:%2X%2X%2X%2X%2X%2X%2X%2X", msg.payload.dest[0], msg.payload.dest[1], msg.payload.dest[2], msg.payload.dest[3], msg.payload.dest[4], msg.payload.dest[5], msg.payload.dest[6], \
+    MYLOG_INFO("src:%2X%2X%2X%2X%2X%2X%2X%2X", msg->payload->dest[0], msg->payload->dest[1], msg->payload->dest[2], msg->payload->dest[3], msg->payload->dest[4], msg->payload->dest[5], msg->payload->dest[6], \
     msg.payload.dest[7]);\
-    MYLOG_INFO("cmdid:%2X%2X", msg.payload.cmdid[0], msg.payload.cmdid[1]);\
-    MYLOG_INFO("groupid:%2X%2X", msg.payload.groupid[0], msg.payload.groupid[1]);\
-    MYLOG_INFO("index:%2X%2X", msg.payload.adf.index[0], msg.payload.adf.index[1]);\
-    MYLOG_INFO("sub:%2X", msg.payload.adf.sub);\
-    MYLOG_INFO("opt:%2X", msg.payload.adf.opt);\
-    MYLOG_INFO("length:%2X", msg.payload.adf.length);\
+    MYLOG_INFO("cmdid:%2X%2X", msg->payload->cmdid[0], msg->payload->cmdid[1]);\
+    MYLOG_INFO("groupid:%2X%2X", msg->payload->groupid[0], msg->payload->groupid[1]);\
+    MYLOG_INFO("index:%2X%2X", msg->payload->adf->index[0], msg->payload->adf->index[1]);\
+    MYLOG_INFO("sub:%2X", msg->payload->adf->sub);\
+    MYLOG_INFO("opt:%2X", msg->payload->adf->opt);\
+    MYLOG_INFO("length:%2X", msg->payload->adf->length);\
     MYLOG_INFO("data:");\
-    MYLOG_BYTE((BYTE*)msg.payload.adf.data, 7+msg.payload.adf.data.length);\
+    MYLOG_BYTE((BYTE*)msg->payload->adf->data, 7+msg->payload->adf->data->length);\
 }
 #endif
