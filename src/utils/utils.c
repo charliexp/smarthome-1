c7e49e44e90d0f39086aa91a2b7a92ea0f2ebfb8
liph1988 <365464919@qq.com>
2019-05-14 13:38:26 +0800

    代码提交
    代码提交

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -30,6 +30,7 @@ extern char g_mac[20];
 char g_current_packetid = 0;
 const char * base64char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 const char padding_char = '=';
+extern FILE* g_fp;
 
 char getpacketid(void)
 {
